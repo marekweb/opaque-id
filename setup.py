6ea7395789ab0ac9a3a1484b6b617a6c48ca50fa
Luke Hutscal <luke@creaturecreative.com>
2011-04-28 11:42:47 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='opaque',
+    version=__import__('opaqure').__version__,
+    description='Obfuscation scheme for integer IDs',
+    author='Marek Z.',
+    url='http://github.com/marekweb/opaque-id',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
